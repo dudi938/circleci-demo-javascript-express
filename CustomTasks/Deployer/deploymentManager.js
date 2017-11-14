ef17ca4323c900ee661efc42048484098d4ed666
dudi938 <dudi938@gmail.com>
2017-11-14 22:02:04 +0200

    add deployer file

---
+++ CustomTasks/Deployer/deploymentManager.js
@@ -0,0 +1,22 @@
+
+
+
+var inputArgs = process.argv.slice(2);
+
+var resourceGroup = inputArgs[0]; 
+var deployType = inputArgs[1]; 
+var gridsCuantity = inputArgs[2]; 
+var firefoxNodsCuantity = inputArgs[3]; 
+var chromeNodsCuantity = inputArgs[4]; 
+var edgeNodsCuantity = inputArgs[5]; 
+var templatePath = inputArgs[6]; 
+var parameterPath = inputArgs[7]; 
+
+console.log('resourceGroup ' + resourceGroup);
+console.log('deployType ' + deployType);
+console.log('gridsCuantity ' + gridsCuantity);
+console.log('firefoxNodsCuantity ' + firefoxNodsCuantity);
+console.log('chromeNodsCuantity ' + chromeNodsCuantity);
+console.log('edgeNodsCuantity ' + edgeNodsCuantity);
+console.log('templatePath ' + templatePath);
+console.log('parameterPath ' + parameterPath);
