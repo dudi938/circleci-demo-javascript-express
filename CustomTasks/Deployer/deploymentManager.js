4b825672eb610eb32743d449545331c56b99e8d0
dudi938 <dudi938@gmail.com>
2017-12-01 15:05:54 +0200

    try add a load balancer

--- CustomTasks/Deployer/deploymentManager.js
+++ CustomTasks/Deployer/deploymentManager.js
@@ -604,15 +604,15 @@ function main() {
 
             var gridsPublicIP = fs.readFileSync(GRID_IP).toString().split('\r\n');
 
-            execCommand('sudo ssh -i ' + PRIVATE_KEY_PATH + '   -oStrictHostKeyChecking=no  yossis@' + gridsPublicIP[0] + ' sh   /home/yossis/configureReplicaset.sh' + ' ' + gridsPublicIP[0] + ' ' + gridsPublicIP[1] + ' ' + gridsPublicIP[2], function () {
+            execCommand('sudo ssh -i ' + PRIVATE_KEY_PATH + '   -oStrictHostKeyChecking=no  testim@' + gridsPublicIP[0] + ' sh   /home/testim/configureReplicaset.sh' + ' ' + gridsPublicIP[0] + ' ' + gridsPublicIP[1] + ' ' + gridsPublicIP[2], function () {
             });
             break;
         case RUN_EDIT_CAMELOT_PROPERTIES:
             var gridsPublicIP = fs.readFileSync(GRID_IP).toString().split('\r\n');
 
-            execCommand('sudo ssh -i ' + PRIVATE_KEY_PATH + '   -oStrictHostKeyChecking=no  yossis@' + gridsPublicIP[0] + ' sh   /home/yossis/editCamelotProperties.sh' + ' ' + gridsPublicIP[0] + ' ' + gridsPublicIP[1] + ' ' + gridsPublicIP[2], function () {
-                execCommand('sudo ssh -i ' + PRIVATE_KEY_PATH + '   -oStrictHostKeyChecking=no  yossis@' + gridsPublicIP[1] + ' sh   /home/yossis/editCamelotProperties.sh' + ' ' + gridsPublicIP[0] + ' ' + gridsPublicIP[1] + ' ' + gridsPublicIP[2], function () {
-                    execCommand('sudo ssh -i ' + PRIVATE_KEY_PATH + '   -oStrictHostKeyChecking=no  yossis@' + gridsPublicIP[2] + ' sh   /home/yossis/editCamelotProperties.sh' + ' ' + gridsPublicIP[0] + ' ' + gridsPublicIP[1] + ' ' + gridsPublicIP[2], function () {
+            execCommand('sudo ssh -i ' + PRIVATE_KEY_PATH + '   -oStrictHostKeyChecking=no  testim@' + gridsPublicIP[0] + ' sh   /home/testim/editCamelotProperties.sh' + ' ' + gridsPublicIP[0] + ' ' + gridsPublicIP[1] + ' ' + gridsPublicIP[2], function () {
+                execCommand('sudo ssh -i ' + PRIVATE_KEY_PATH + '   -oStrictHostKeyChecking=no  testim@' + gridsPublicIP[1] + ' sh   /home/testim/editCamelotProperties.sh' + ' ' + gridsPublicIP[0] + ' ' + gridsPublicIP[1] + ' ' + gridsPublicIP[2], function () {
+                    execCommand('sudo ssh -i ' + PRIVATE_KEY_PATH + '   -oStrictHostKeyChecking=no  testim@' + gridsPublicIP[2] + ' sh   /home/testim/editCamelotProperties.sh' + ' ' + gridsPublicIP[0] + ' ' + gridsPublicIP[1] + ' ' + gridsPublicIP[2], function () {
                     });
                 });
             });
